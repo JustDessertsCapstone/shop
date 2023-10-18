ba55493982924de97f383a5e8c127a0307239301
Riley Mapel <rileymapel@gmail.com>
2023-10-18 00:33:02 -0500

    removed unused react/vite stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import data from './assets/products.json'
 import ProductContainer from './ProductContainer'
@@ -20,15 +18,6 @@ function App() {
 
       <main>
         <h1> Hello, welcome to Healthify</h1>
-        <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        </div>
-        <h1>Vite + React</h1>
         <div>
           <ProductContainer data={data} />
         </div>
