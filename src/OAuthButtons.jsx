475f27bb2b083df0dcb767501c70430e2dd1df06
Riley Mapel <rileymapel@gmail.com>
2023-11-08 21:55:35 -0600

    corrected UserState

--- src/OAuthButtons.jsx
+++ src/OAuthButtons.jsx
@@ -2,48 +2,50 @@ import { jwtDecode } from "jwt-decode"
 import { useState } from "react";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
+function LoginButton({ setUser }) {
+
+  const onSuccess = (res) => {
+    setUser(jwtDecode(res["credential"]));
+  };
+
+  const onFailure = (res) => {
+    console.log("Login Failed");
+  };
+  
+  return (
+    <GoogleLogin id="signInButton" onSuccess={onSuccess} onError={onFailure} />
+  );
+}
+
+function LogoutButton({ setUser }) {
+  const signOut = () => {
+    googleLogout();
+    setUser([]);
+  };
+
+  const onSuccess = () => {
+    console.log("Logout Successful");
+  };
+
+  return (
+    <button id="signOutButton" onClick={signOut}>
+      Sign Out
+    </button>
+  );
+}
+
 export function UserState() {
   const [ user, setUser ] = useState([]);
 
   return [ user, setUser ];
 }
 
-export function OAuthButtons(setUser) {
-  const LoginButton = () => {
-    const onSuccess = (res) => {
-      setUser(jwtDecode(res["credential"]));
-    };
-  
-    const onFailure = (res) => {
-      console.log("Login Failed");
-    };
-    
-    return (
-      <GoogleLogin id="signInButton" onSuccess={onSuccess} onError={onFailure} />
-    );
-  }
-
-  const LogoutButton = () => {
-    const signOut = () => {
-      googleLogout();
-    };
-  
-    const onSuccess = () => {
-      console.log("Logout Successful");
-    };
-  
-    return (
-      <button id="signOutButton" onClick={signOut}>
-        Sign Out
-      </button>
-    );
-  }
-
+export function OAuthButtons({ setUser }) {
   return (
     <div className="oath-buttons">
       <p>Please Sign In:</p>
-      <LoginButton />
-      {/* <LogoutButton /> */}
+      <LoginButton setUser={setUser} />
+      {/* <LogoutButton setUser={setUser} /> */}
     </div>
   );
 }
